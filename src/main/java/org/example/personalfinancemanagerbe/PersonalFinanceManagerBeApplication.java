c8e9bd30e53896fb51f448f89e84a4c6d83d760d
arut3n <arut3ncodes@gmail.com>
2026-08-13 13:09:29 +0200

    Repo init

---
+++ src/main/java/org/example/personalfinancemanagerbe/PersonalFinanceManagerBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.personalfinancemanagerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceManagerBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalFinanceManagerBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/personalfinancemanagerbe/PersonalFinanceManagerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.personalfinancemanagerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceManagerBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
