4ff59f2464100f6370ea3923b4355524080055ff
arut3n <arut3ncodes@gmail.com>
2026-09-16 14:07:29 +0200

    Added and configured JPA + H2 database, modified TransactionModel to Jpa entity and modified field annotations.

--- src/main/java/org/example/personalfinancemanagerbe/models/TransactionModel.java
+++ src/main/java/org/example/personalfinancemanagerbe/models/TransactionModel.java
@@ -1,20 +1,31 @@
 package org.example.personalfinancemanagerbe.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.personalfinancemanagerbe.util.TransactionType;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
-//@Entity(name="Transaction")
+@Entity()
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Table(name="transaction")
 public class TransactionModel {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="transaction_id")
     private Long id;
-    private Double amount;
+    @Column(name="amount", nullable = false, precision = 16, scale=2)
+    private BigDecimal amount;
+    @Column(name="description", length = 256)
     private String description;
+    @Column(name="date", nullable = false)
     private LocalDate date;
+    @Column(name="type", nullable = false)
+    @Enumerated(EnumType.STRING)
     private TransactionType type;
 }
