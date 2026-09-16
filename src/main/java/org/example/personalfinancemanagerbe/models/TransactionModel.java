552036b517951bba5cb0e4574a2f791b923bbf6c
arut3n <arut3ncodes@gmail.com>
2026-09-16 15:31:07 +0200

    Polished field annotations, refactored repository to use JpaRepository

--- src/main/java/org/example/personalfinancemanagerbe/models/TransactionModel.java
+++ src/main/java/org/example/personalfinancemanagerbe/models/TransactionModel.java
@@ -1,15 +1,20 @@
 package org.example.personalfinancemanagerbe.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.personalfinancemanagerbe.util.TransactionType;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Entity()
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -19,13 +24,22 @@ public class TransactionModel {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     @Column(name="transaction_id")
     private Long id;
+
     @Column(name="amount", nullable = false, precision = 16, scale=2)
+    @NotNull
+    @Digits(integer=14, fraction=2)
     private BigDecimal amount;
+
+    @Length(max = 256)
     @Column(name="description", length = 256)
     private String description;
+
     @Column(name="date", nullable = false)
+    @NotNull
     private LocalDate date;
+
     @Column(name="type", nullable = false)
+    @NotNull
     @Enumerated(EnumType.STRING)
     private TransactionType type;
 }
